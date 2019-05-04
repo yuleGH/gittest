84928a7cdf9b54052b1043aa42e8df6533723024
yule.mail@qq.com <yule.mail@qq.com>
2019-05-04 21:31:35 +0800

    dev 提交

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,7 +1,7 @@
 package test;
 
 /**
- * 测试
+ * 测试123
  * @author yule
  * @date 2019/5/4 16:03
  */
@@ -9,6 +9,6 @@ public class Test {
     public static void main(String[] args){
         System.out.println(11121);
         System.out.println(2223);
-        System.out.println(55);
+        System.out.println(545);
     }
 }
